b69095f821f0f9db82ee7f7d7fa3a548a59dfc83
Lawrence Anderson <anderson0741@icloud.com>
2018-03-12 11:43:27 -0600

    remove serverside

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const portfolioRoutes = express.Router();
-const Portfolios = require('../models/models');
+// const express = require('express');
+// const portfolioRoutes = express.Router();
+// const Portfolios = require('../models/models');
 
-portfolioRoutes.get('/', (req, res) => {
-    Portfolios.find({}, (err, portfolioz) => {
-        if(err) return res.status(500).send(err);
-        return res.send(portfolioz);
-    })
-});
+// portfolioRoutes.get('/', (req, res) => {
+//     Portfolios.find({}, (err, portfolioz) => {
+//         if(err) return res.status(500).send(err);
+//         return res.send(portfolioz);
+//     })
+// });
 
-portfolioRoutes.post('/', (req, res) => {
-    const newPortfolio = new Portfolios(req.body);
-    portfolio.user = req.user;
-    newPortfolio.save((err) => {
-        if (err) return res.status(500).send(err);
-        return res.send(newPortfolio);
-    })
-});
+// portfolioRoutes.post('/', (req, res) => {
+//     const newPortfolio = new Portfolios(req.body);
+//     portfolio.user = req.user;
+//     newPortfolio.save((err) => {
+//         if (err) return res.status(500).send(err);
+//         return res.send(newPortfolio);
+//     })
+// });
 
-portfolioRoutes.get('/:id', (req, res) => {
-    Portfolios.findById(req.params.id, (err, portfolio) => {
-        if (err) return res.status(500).send(err);
-        return res.send(portfolio);
-    })
-});
+// portfolioRoutes.get('/:id', (req, res) => {
+//     Portfolios.findById(req.params.id, (err, portfolio) => {
+//         if (err) return res.status(500).send(err);
+//         return res.send(portfolio);
+//     })
+// });
 
-portfolioRoutes.put('/:id', (req, res) => {
-    Portfolios.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedPortfolio) => {
-        if (err) return res.status(500).send(err);
-        return res.send(updatedPortfolio);
-    })
-});
+// portfolioRoutes.put('/:id', (req, res) => {
+//     Portfolios.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedPortfolio) => {
+//         if (err) return res.status(500).send(err);
+//         return res.send(updatedPortfolio);
+//     })
+// });
 
-portfolioRoutes.delete('/:id', (req, res) => {
-    console.log(req.params.id)
-    Portfolios.findByIdAndRemove(req.params.id, (err, deletedPortfolio) =>{
-        if (err) return res.status(500).send(err);
-        return res.send(deletedPortfolio);
-    })
-});
+// portfolioRoutes.delete('/:id', (req, res) => {
+//     console.log(req.params.id)
+//     Portfolios.findByIdAndRemove(req.params.id, (err, deletedPortfolio) =>{
+//         if (err) return res.status(500).send(err);
+//         return res.send(deletedPortfolio);
+//     })
+// });
 
-module.exports = portfolioRoutes;
+// module.exports = portfolioRoutes;
