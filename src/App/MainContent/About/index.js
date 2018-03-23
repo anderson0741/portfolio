f4d1b50d3bab5693c4b8d47c496d7d0db3d7ddec
Lawrence Anderson <anderson0741@icloud.com>
2018-03-22 20:32:50 -0600

    text

--- src/App/MainContent/About/index.js
+++ src/App/MainContent/About/index.js
@@ -17,7 +17,7 @@ function About(props) {
                     <img className='picture_border' src={Picture_Of_Me} alt="" />
                 </div>
                 <div className="theAbout">
-                    <h1>Web Developer with a background in sales and client relations creating hundreds of accounts over a 5 year period and maintaining one of the company's highest retention rates. Looking to apply recent training in front end development including JavaScript and React. Eager to learn new programming languages. </h1>
+                    <h1>Web Developer with a background in sales and client relations creating hundreds of accounts over a 5 year period and maintaining one of the company's highest retention rates. Looking to apply recent training in front end development including JavaScript and React. Eager to learn new programming languages.</h1>
                 </div>
             </div>
         </div>
