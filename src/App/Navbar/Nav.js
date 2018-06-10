70d2da99291aa6b421116ccd368aead91cf3ac9c
Lawrence Anderson <anderson0741@icloud.com>
2018-06-09 22:22:10 -0600

    port work

--- src/App/Navbar/Nav.js
+++ src/App/Navbar/Nav.js
@@ -22,13 +22,15 @@ function Nav() {
                 <div className="navBod_level">
                     <button className="dropbtn">Projects</button>
                     <div className="level_up_dropdwn">
-                        <a className="titleLink" href="/level_up_home">Level Up</a>
+                        <a className="titleLink" href="http://levelupcarsales.com/">Level Up</a>
                         <a href="/level_up_home">Home</a>
                         {/* <a href="/level_up_about">About</a> */}
                         <a href="/level_up_create">Upload Content</a>
                         <a href="/level_up_shopping">Shop</a>
                         <a href="/level_up_contact">Contact Us</a>
                         <a href="/level_up_login">Login</a>
+                        <a className="titleLink" href="http://www.europeandobes.com/">European Dobermans</a>
+                        <a className="titleLink" href="https://big-eyez.herokuapp.com/#home">Big Eyes</a>
                         <a className="titleLink" href="https://timer-for-portfolio.herokuapp.com/end.html">Small Projects</a>
                         {/* <a href="/level_up_about">About</a> */}
                         <a href="https://timer-for-portfolio.herokuapp.com/end.html">Clock CSS/JS</a>
